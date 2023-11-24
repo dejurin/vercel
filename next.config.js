f711d6a4b05a5a5d98b3041a8f9a459f58e18598
YURII D <gkhelloworld@gmail.com>
2023-11-25 00:02:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-const locales = ["en", "es"];
-
-const pages = locales.map((locale) => ({
-  source: "/:path*",
-  destination: `/${locale}/:path*`,
-  has: [
-    {
-      type: "host",
-      value: "vercel-git-main-dejurin.vercel.app",
-    },
-  ],
-}));
-
-const afterFiles = [];
-
-afterFiles.push(...pages);
+const nextConfig = {
+  async rewrites() {
+    return {
+      afterFiles: [{
+        source: "/:path*",
+        destination: `/en/:path*`,
+        has: [
+          {
+            type: "host",
+            value: "vercel-git-main-dejurin.vercel.app",
+          },
+        ],
+      }],
+    };
+  },
+};
 
 module.exports = nextConfig;
